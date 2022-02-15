5ebcc9e24e8f20e84879ecea97b74db2ce916b6d
hanhvn <vonhathanh@gmail.com>
2022-02-15 11:01:16 +0700

    Parse command line args and read config file

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import argparse
+import configparser
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from config import Config
 
 
-# Press the green button in the gutter to run the script.
+def read_config(network: str) -> Config:
+    parser = configparser.ConfigParser()
+    parser.read("config.ini")
+
+    config_section = parser[network]
+
+    cfg = Config()
+    cfg.ROUTER_ADDRESS = config_section["ROUTER"]
+    cfg.BUSD_ADDRESS = config_section["BUSD"]
+    cfg.CWIG_ADDRESS = config_section["CWIG"]
+    cfg.CWIG_BUSD_LP_ADDRESS = config_section["CWIG_BUSD_LP"]
+
+    return cfg
+
+
+def run(arg: dict, cfg: Config):
+    pass
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    arg_parse = argparse.ArgumentParser()
+    arg_parse.add_argument("low_price", type=float, help="lower bound of cwig-busd price")
+    arg_parse.add_argument("high_price", type=float, help="upper bound of cwig-busd price")
+    arg_parse.add_argument("--network", type=str, required=False, default="testnet", choices=["mainnet", "testnet"],
+                           help="which network to run")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    args = vars(arg_parse.parse_args())
+    config = read_config(args["network"])
+
+    run(args, config)
+
+    print(args)
+    print(config.ROUTER_ADDRESS, config.CWIG_ADDRESS, config.BUSD_ADDRESS, config.CWIG_BUSD_LP_ADDRESS)
